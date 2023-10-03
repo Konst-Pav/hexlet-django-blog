1735c3eba9a3dcaa48b3c43f54c5d848071ac1ab
kpavlov <k.pavlov.reg@yandex.ru>
2023-10-03 18:02:28 +0300

    update

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from hexlet_django_blog.article import views
+from hexlet_django_blog.article.views import IndexView, ArticleView
 
 
 urlpatterns = [
-    path('', views.index),
-    path('<int:user_id>/', views.article, name='article_user_id'),
+    path('', IndexView.as_view()),
+    path('<int:id>/', ArticleView.as_view(), name='article_view'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,16 +1,27 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views.decorators.http import require_http_methods
 from django.http import Http404
+from hexlet_django_blog.article.models import Article
 
 
 class ArticleView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'articles/index.html', context={'name': 'Articles'})
+        article = get_object_or_404(Article, id=kwargs['id'])
+        return render(request, 'articles/show.html', context={
+            'article': article,
+        })
 
 
-# Create your views here.
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        articles = Article.objects.all()[:15]
+        return render(request, 'articles/index.html', context={
+            'articles': articles,
+        })
+
+
 def index(request):
     return render(request, 'articles/index.html', context={'name': 'Articles'})
 
@@ -19,6 +30,6 @@ def index(request):
 def article(request, user_id):
     if request.method == 'GET':
         path = reverse('article_user_id', kwargs={'user_id': user_id})
-        return render(request, 'articles/article.html', context={'user_id': user_id, 'path': path})
+        return render(request, 'articles/show.html', context={'user_id': user_id, 'path': path})
     else:
         raise Http404()
