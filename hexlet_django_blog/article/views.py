3229443b8f4be11e2ea6d40d2340402accb5efb7
kpavlov <k.pavlov.reg@yandex.ru>
2023-10-24 16:15:52 +0300

    update

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,16 +1,20 @@
 from django.views import View
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.views.decorators.http import require_http_methods
 from django.http import Http404
+from django.contrib import messages
 from hexlet_django_blog.article.models import Article
+from .forms import ArticleForm
 
 
 class ArticleView(View):
     def get(self, request, *args, **kwargs):
         article = get_object_or_404(Article, id=kwargs['id'])
+        article_id = kwargs.get('id')
         return render(request, 'articles/show.html', context={
             'article': article,
+            'article_id': article_id,
         })
 
 
@@ -22,14 +26,63 @@ class IndexView(View):
         })
 
 
-def index(request):
-    return render(request, 'articles/index.html', context={'name': 'Articles'})
+class ArticleFormEditView(View):
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(instance=article)
+        return render(request, 'articles/update.html', {
+            'form': form,
+            'article_id': article_id,
+        })
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Article updated')
+            return redirect('articles')
+        messages.error(request, 'Invalid data')
+        return render(request, 'articles/update.html', {
+            'form': form,
+            'article_id': article_id,
+        })
+
+
+class ArticleFormCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'articles/create.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Article created')
+            return redirect('articles')
+        messages.error(request, 'Invalid data')
+        return render(request, 'articles/create.html', {'form': form})
+
+
+class ArticleFormDeleteView(View):
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        if article:
+            article.delete()
+            messages.success(request, 'Article deleted')
+        return redirect('articles')
 
 
 @require_http_methods(['GET', 'POST'])
 def article(request, user_id):
     if request.method == 'GET':
         path = reverse('article_user_id', kwargs={'user_id': user_id})
-        return render(request, 'articles/show.html', context={'user_id': user_id, 'path': path})
+        return render(request, 'articles/show.html', {
+            'user_id': user_id,
+            'path': path,
+        })
     else:
         raise Http404()
